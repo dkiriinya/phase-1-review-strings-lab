7508fbde54475950174ef6f7fe1b17944c3360ff
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-11 23:06:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+let currentUser = 'Don'
+
+let welcomeMessage = "Welcome to Flatbook, "
+welcomeMessage = welcomeMessage + currentUser + "!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+excitedWelcomeMessage = excitedWelcomeMessage + currentUser.toUpperCase() + "!"
+
+let shortGreeting = "Welcome, "
+shortGreeting = shortGreeting + currentUser.slice(0,1) + "!"
+
+
